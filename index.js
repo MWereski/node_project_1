049b2604f50ff09b17458c4a6aeed9ef1cf1c16a
MWereski <michalwereskimw@gmail.com>
2021-12-22 21:11:21 +0100

    last changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
+const usersRoutes = require('./routes/users')
 
 const app = express();
 
@@ -29,80 +29,4 @@ app.use(cookieParser());
 
 //     console.log("ELp: " + name)
 // });
-
-
-app.post('/hello', (req, res) => {
-    const {name, surname} = req.body;
-    console.log(name, surname)
-    res.send('Witaj ' + name + ' ' + surname)
-
-})
-
-app.get('/hi/:name', (req, res) => {
-    const {name} = req.params;
-    const dt = new Date();
-    dt.setDate(dt.getDate() + 7);
-
-    res.cookie('visitor_name', name, {
-        expires: dt,
-    });
-
-    res.send('Imię zapisano.')
-
-});
-
-app.get('/logout', (req, res) => {
-    res.clearCookie('visitor_name');
-
-    res.redirect('/')
-
-})
-
-
-app.get('/', (req, res) => {
-
-    res.send('Strona główna');
-
-    // const fileName = 'elo.html';
-    // res.sendFile(fileName, {
-    //     root: path.join(__dirname, 'front')
-    // });
-    
-});
-
-app.get('/logo', (req, res) => {
-    
-
-    const fileName = path.join(__dirname, 'front/unnamed.jpg');
-    // res.sendFile(fileName, {
-    //     root: path.join(__dirname, 'front')
-    // });
-
-    res.download(fileName, 'ELOPLIKKO');
-
-    
-});
-
-app.post('/', (req) => {
-    console.log('Dodawanie nowej osoby')
-
-})
-
-app.get('/:id', (req, res) => {
-    console.log('Szczrgół o osobie id: ' + req.params.id)
-
-   // res.send('Hello World: ' + req.params.id)
-   //res.location('localhot:3000')
-   res.redirect('https://www.google.pl')
-   //res.sendStatus(302);
-});
-
-app.patch('/:id', (req) => {
-    console.log('Aktualizacja osoby id: ' + req.params.id)
-
-})
-
-app.delete('/:id', (req) => {
-    console.log('Usuwanie osoby o id: ' + req.params.id)
-
-})
+usersRoutes(app)
